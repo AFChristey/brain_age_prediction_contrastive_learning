ee8e13b6c5d32e8d8ff0e2e522e1ad56b99d84d7
alex christey <alex@dhcp-10-248-129-220.eduroam.wireless.private.cam.ac.uk>
2025-02-14 16:27:18 +0000

    testing

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,4 @@
-from .resnet3d import SupConResNet, SupRegResNet, SupCEResNet, LinearRegressor
+from .resnet3d import SupConResNet
 from .alexnet3d import SupConAlexNet, SupRegAlexNet
 from .densenet3d import SupConDenseNet, SupRegDenseNet
 from .estimators import AgeEstimator, SiteEstimator
