6ffe1efe27bde15f56cf77947565b22de174aac0
alex christey <alex@alexchrsteysair.lan>
2025-03-30 17:57:14 +0100

    testing

--- src/main_infonce.py
+++ src/main_infonce.py
@@ -1067,15 +1067,15 @@ if __name__ == '__main__':
         sweep_config = {
             'method': 'random',
             # "name": "classification_tuning_dynamic_negative_classloss_noGRL_part2",
-            "name": "tuning_of_mmd",
+            "name": "tuning_of_mmd_RnC_OpenBHB",
             'metric': {
                 'name': 'train/mae', #'mae_train'
                 'goal': 'minimize'
             },
             "parameters": {
             # "batch_size": {"values": [32, 64]},
-            "lr": {"values": [1e-4, 1e-2, 1e-5, 1e-3]},
-            "weight_decay": {"values": [1e-6, 1e-2, 1e-4]},
+            # "lr": {"values": [1e-4, 1e-2, 1e-5, 1e-3]},
+            # "weight_decay": {"values": [1e-6, 1e-2, 1e-4]},
             # # "temp": {"values": [0.05, 0.1, 0.2]},
             # # "method": {"values": ["supcon", "yaware"]},
             # # "optimizer": {"values": ["adam", "sgd"]},
@@ -1092,7 +1092,7 @@ if __name__ == '__main__':
             # # "grl_layer": {"values": [1, 0]},
             # "lambda_val": {"values": [0.0005, 0.005, 0.05, 0.5, 5, 50]}
             # "trial": {"values": [0,1,2,3,4]},
-            "lambda_mmd": {"values": [1e-6, 1e-4, 1e-5, 1e-2, 1e-1, 1, 10, 100]},
+            "lambda_mmd": {"values": [1e-6, 1e-4, 1e-5, 1e-2, 1e-1, 1e-3, 10, 100]},
 
             # lambda_mmd
 
